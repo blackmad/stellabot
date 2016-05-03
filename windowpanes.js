c54fae4eaedf78ef665d840d5bfb6c7a91d062e2
David Blackman <david@blackmad.com>
2016-05-03 08:48:15 -0400

    totally broken but close

--- windowpanes.js
+++ windowpanes.js
@@ -17,6 +17,12 @@ if (typeof module !== 'undefined' && module.exports) {
   }
 }
 
+Math.seed = function(s) {
+  return function() {
+    s = Math.sin(s) * 10000; return s - Math.floor(s);
+  };
+};
+
 function getRndColor() {
     var r = 255*Math.random()|0,
         g = 255*Math.random()|0,
@@ -30,6 +36,10 @@ function randInt (low, high) {
 
 var colors = null;
 var colorIndex = null;
+
+function makeColors() {
+  return  tinycolor(getRndColor()).analogous(slices = 5, results = 40).map(function(t) { return t.toHexString(); });
+}
 
 function initColors() {
   colorOptions = [
@@ -46,8 +56,7 @@ function initColors() {
   var colorFunctions = ['lighten'] // , 'darken', 'brighten', 'saturate', 'desaturate']
   var colorFunction = _.sample(colorFunctions)
   colors = colors.map(function(c) { return c.lighten(20); });
-
-  colors = colors.map(function(t) { return t.toHexString(); })
+  colors = colors.map(function(t) { return t.toHexString(); });
 
   colorIndex = 0;
 }
@@ -89,33 +98,49 @@ function draw_circles(ctx, colors, radius, band_width, padding) {
   ctx.rect(0, 0, radius, radius);
   ctx.clip();
 
+  var radiiAndColors = []
+  var currentRadius = band_width
+  var currentColorIndex = 0
+  while (currentRadius < radius) {
+    radiiAndColors.push([currentRadius, colors[currentColorIndex % colors.length]])
+    currentRadius += band_width;
+    currentColorIndex += 1
+  }
+  radiiAndColors.reverse()
+  console.log(radiiAndColors)
+
+  console.log('\n\n circles!!!!')
+  console.log('drawing with colors: ' + colors)
+
   var iterations = Math.floor(radius / band_width)
-  _.times(iterations, function() {
-    if (radius > 0) {
-      ctx.fillStyle = getRndColor();
-      ctx.beginPath();
-      ctx.arc(0,0,radius, 0,Math.PI);
-      ctx.fill();
+  _.each(radiiAndColors, function(radiusAndColor, index) {
+    var radius = radiusAndColor[0]
+    var color = radiusAndColor[1]
+    console.log('drawing circle ' + index + ' with color ' + color + ' & radius: ' + radius)
+    ctx.fillStyle = 
+    ctx.beginPath();
+    ctx.arc(padding,padding,radius-padding, 0,Math.PI);
+    ctx.fill();
 
-      ctx.strokeStyle = '#ffffff'
-      ctx.lineWidth = padding;
-      ctx.beginPath();
-      ctx.arc(0,0,radius, 0,Math.PI);
-      ctx.stroke();
+    // ctx.strokeStyle = '#ffffff'
+    // ctx.lineWidth = padding;
+    // ctx.beginPath();
+    // ctx.arc(padding, padding,radius-padding, 0,Math.PI);
+    // ctx.stroke();
 
-      radius -= band_width
-    }
+    radius -= band_width
   })
 }
 
-function draw_pane(ctx, size) {
-  arc_size = 10
+function draw_pane(ctx, size, colors1, colors2) {
+  arc_size = size * 0.12
+  border_size = size * 0.10
 
   ctx.save()
   ctx.beginPath();
   ctx.rect(0, 0, size, size);
   ctx.clip();
-  draw_circles(ctx, colors, size * 2, arc_size, 1)
+  draw_circles(ctx, colors1, size * 2, arc_size, 1)
   ctx.restore()
 
   ctx.save()
@@ -124,20 +149,22 @@ function draw_pane(ctx, size) {
   ctx.clip();
   rotate90(ctx, size)
   rotate90(ctx, size)
-  draw_circles(ctx, colors, size * 0.82, arc_size, 1)
+  draw_circles(ctx, colors2, size * 0.82, arc_size, 1)
   ctx.restore()
 
   ctx.beginPath()
-  ctx.rect(0, 0, size, size)
+  ctx.rect(border_size/2, border_size/2, size - border_size, size - border_size)
   ctx.strokeStyle = getRndColor()
   ctx.lineWidth = size * 0.10
   ctx.stroke();
-
-
 }
 
 
 function draw_everything(canvas, forceGlitch) {
+  // var seed = new Date().getTime()
+  var seed = 100
+  Math.random = Math.seed(seed)
+
   initColors();
 
   var ctx = canvas.getContext('2d');
@@ -147,11 +174,24 @@ function draw_everything(canvas, forceGlitch) {
   ctx.moveTo(0, 0)
 
   ctx.save()
-  ctx.translate(300, 300)
+  ctx.translate(50, 50)
 
-  draw_pane(ctx, 100)
+  var colors1 = makeColors()
+  var colors2 = makeColors()
 
-  
+  ctx.save()
+  draw_pane(ctx, 300, colors1, colors2)
+  ctx.restore()
+
+  // // flip vertical
+  // ctx.scale(-1, 1)
+  // ctx.translate(-300, 300)
+
+  // flip horizontal
+  ctx.scale(1, -1)
+  ctx.translate(300, -300)
+
+  draw_pane(ctx, 300, colors2, makeColors())
 
   ctx.restore()
 
