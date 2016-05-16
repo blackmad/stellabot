432e92a8f53b07bb4f12ad5ce437a9ac4149b11e
David Blackman <dblackman@twitter.com>
2016-05-16 07:21:12 -0400

    move around web demos

--- web/common.js
+++ web/common.js
@@ -1,3 +1,10 @@
+function resize_canvas(){
+  canvas = document.getElementById("myCanvas");
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+  draw_everything(canvas, false);
+}
+
 $(document).ready(function() {
   var html = '<input type="button" class="redraw btn" id="redraw" value="redraw"/>' +
     '<canvas id="myCanvas" width="100%" height="100%" style="border:1px solid #000000;"/>'
@@ -10,19 +17,10 @@ $(document).ready(function() {
     '<canvas id="myCanvas" width="100%" height="100%" style="border:1px solid #000000;"/>'
   ));
 
-  $('body').resize(resize_canvas);
-
-  $('#redraw').click(resize_canvas);
-
-  function resize_canvas(){
-    canvas = document.getElementById("myCanvas");
-    canvas.width  = window.innerWidth;
-    canvas.height = window.innerHeight;
-    draw_everything(canvas, false);
-  }
+  $(window).on("resize", resize_canvas);
+  $('#redraw').on("click", resize_canvas);
 
    //setTimeout(draw, 2500);
 
-  console.log('resizing');
   resize_canvas();
 });
