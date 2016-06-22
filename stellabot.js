553ee1e38c7a68b96b3407f4462fae30888f86ac
David Blackman <dblackman@twitter.com>
2016-06-22 14:26:45 -0400

    fix runner

--- stellabot.js
+++ stellabot.js
@@ -19,7 +19,10 @@ var draw_modules = [
 ]
 
 var draw_module = _.sample(draw_modules);
-draw_module.draw_everything(canvas, argv['glitch'] || false);
+draw_module.draw_everything({
+  canvas: canvas, 
+  always_glitch: argv['glitch'] || false
+});
 
 Math.seed = function(s) {
   return function() {
